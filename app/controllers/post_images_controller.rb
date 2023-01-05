e55b6a64c84b8a914a01b2a1534c11ae48101140
Jihye19249205 <119666066+Jihye19249205@users.noreply.github.com>
2023-01-05 14:06:34 +0000

    [uodate]4-16

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -22,6 +22,8 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to '/post_images'
+  end
+  
   private
   
   def post_image_params
